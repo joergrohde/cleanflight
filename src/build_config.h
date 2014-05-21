81ab9a08ef053476bd992fe3f676e0bc3171cc3c
Dominic Clifton <me@dominicclifton.name>
2014-05-21 06:35:39 +0100

    Enable software and disable hardware I2C driver. Software one has been tested and works, hardware causes issues with new PWM code.
    Conflicts:
    	src/board.h

--- src/build_config.h
+++ src/build_config.h
@@ -2,7 +2,7 @@
 
 #define BUILD_BUG_ON(condition) ((void)sizeof(char[1 - 2*!!(condition)]))
 
-//#define SOFT_I2C // enable to test software i2c
+#define SOFT_I2C
 
 #ifndef __CC_ARM
 #define REQUIRE_CC_ARM_PRINTF_SUPPORT
